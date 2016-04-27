136f57590718c46cfe950d48db9c92c3f26f47aa
Antony Donovan <antony.donovan@generalassemb.ly>
2016-04-26 22:33:57 -0400

    Update template to use mocha
    Eliminate jasmine and "browser" testing.

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const chai = require('chai');
+
+const expect = chai.expect;
+
 const example = require('../lib/example');
 
-describe('Example', () => {
-  it('is true', () => {
-    expect(example()).toBe(true);
-  });
-});
+describe('Example', () =>
+  it('is true', () =>
+    expect(example()).to.be.true
+  )
+);
